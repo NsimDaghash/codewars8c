06de5c409ec3561570b638ae1f1a9496bfff9b2a
NsimDaghash <89819123+NsimDaghash@users.noreply.github.com>
2022-11-25 15:43:33 +0200

    Remove duplicates from list
    Define a function that removes duplicates from an array of numbers and returns it as a result.
    
    The order of the sequence has to stay the same.

---
+++ RemoveDuplicatesFromList.js
@@ -0,0 +1,16 @@
+/* Remove duplicates from list */
+/*
+Define a function that removes duplicates from an array of numbers and returns it as a result.
+
+The order of the sequence has to stay the same.
+*/
+function distinct(a) {
+  let result=[];
+  for ( let i=0; i< a.length;i++){
+    if (!result.includes(a[i])){
+        result.push(a[i])
+        }
+  }
+  a=result;
+  return a;
+}
