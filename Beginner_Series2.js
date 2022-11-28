f020577c8b4aaa0bdf03289633eb6d8cd5326b5f
NsimDaghash <89819123+NsimDaghash@users.noreply.github.com>
2022-11-28 23:45:23 +0200

    Add files via upload

---
+++ Beginner_Series2.js
@@ -0,0 +1,22 @@
+/* 
+Beginner Series #2 Clock
+Clock shows h hours, m minutes and s seconds after midnight.
+
+Your task is to write a function which returns the time since midnight in milliseconds.
+
+Example:
+h = 0
+m = 1
+s = 1
+
+result = 61000
+Input constraints:
+
+0 <= h <= 23
+0 <= m <= 59
+0 <= s <= 59
+*/
+function past(h, m, s){
+  //#Happy Coding! ^_^
+ return ((s+m*60+h*60*60)*1000);
+}
